7c40e5e795d5922e5f7a96c9e672468bfb8e7885
AndresALPD <andresalpizar03@gmail.com>
2025-08-09 17:39:49 -0600

    Minimal update

---
+++ src/main/resources/db/migration/V2__update_all_passwords.sql
@@ -0,0 +1,3 @@
+UPDATE usuarios SET password = '$2a$10$ZXjUQO53EsR6loPz81lyS.Qvmg2ki1BfM6Rw6QTgJt3bXNULDXqVO' WHERE email = 'admin@forohub.com';
+UPDATE usuarios SET password = '$2a$10$ZXjUQO53EsR6loPz81lyS.Qvmg2ki1BfM6Rw6QTgJt3bXNULDXqVO' WHERE email = 'juan.perez@email.com';
+UPDATE usuarios SET password = '$2a$10$ZXjUQO53EsR6loPz81lyS.Qvmg2ki1BfM6Rw6QTgJt3bXNULDXqVO' WHERE email = 'maria.gonzalez@email.com';
